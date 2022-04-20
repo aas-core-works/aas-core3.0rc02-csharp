d79e7daac70716adeaa8ede476d9de2528152de1
Marko Ristin <marko.ristin@gmail.com>
2022-04-20 10:08:38 +0200

    Kick off
    This is the initial version of the SDK.
    
    So far, we generated the code with aas-core-codegen 0.0.14 based on
    aas-core-meta 2022.6.19. The test data has been generated with
    aas-core3.0rc02-testgen.
    
    The test data involves only JSON data. The tests with XML are still
    pending. The functions on the structures have not been tested (such as
    ``Descend``, ``DescendOnce`` or ``XorDefault``).

---
+++ src/Check.ps1
@@ -0,0 +1,39 @@
+#!/usr/bin/env pwsh
+
+<#
+This script runs locally the checks from the continous integration.
+#>
+
+function Main
+{
+    Push-Location
+
+    Set-Location $PSScriptRoot
+
+    $nl = [System.Environment]::NewLine
+
+    Write-Host "Check.ps1: Checking the format...$nl"
+    dotnet format --verify-no-changes
+    if ($LASTEXITCODE -ne 0)
+    {
+        throw "Format check failed."
+    }
+
+    Write-Host "${nl}Check.ps1: Running the unit tests...${nl}"
+    dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+    if ($LASTEXITCODE -ne 0)
+    {
+        throw "The unit tests failed."
+    }
+
+    Write-Host "${nl}Check.ps1: Inspecting the code with JetBrains InspectCode...${nl}"
+    & "$PSScriptRoot\InspectCode.ps1"
+
+    $outDir = Join-Path (Split-Path -Parent $PSScriptRoot) "out"
+    Write-Host "${nl}Check.ps1: Publishing to $outDir ... ${nl}"
+    dotnet publish -c Release -o $outDir
+
+    Pop-Location
+}
+
+Main
