65071487d97c0b87ddd5ed4e1739c2e5c45a2a15
Marko Ristin <marko.ristin@gmail.com>
2022-06-24 15:46:22 +0200

    Test ``VisitorThrough``
    We compare it against ``Descend``.
    
    We had to make methods in the ``VisitorThrough`` virtual, which is
    reflected in [aas-core-codegen 24690df].
    
    [aas-core-codegen 24690df]: https://github.com/aas-core-works/aas-core-codegen/commit/24690df

---
+++ src/AasCore.Aas3_0_RC02.Tests/TestDescendAndVisitorThrough.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections.Generic;
+using Directory = System.IO.Directory;
+using Path = System.IO.Path;
+
+using System.Linq; // can't alias
+using NUnit.Framework; // can't alias
+
+using Aas = AasCore.Aas3_0_RC02;
+
+namespace AasCore.Aas3_0_RC02.Tests
+{
+    public class TestDescendAndVisitorThrough
+    {
+        private static readonly string JsonExpectedDir = Path.Combine(
+            AasCore.Aas3_0_RC02.Tests.Common.OurTestResourceDir,
+            "Json",
+            "Expected");
+
+        /// <summary>
+        /// Find all the complete examples in the <see cref="JsonExpectedDir"/>.
+        /// </summary>
+        private static List<string> PathsToCompleteExamples()
+        {
+            if (!Directory.Exists(JsonExpectedDir))
+            {
+                throw new System.InvalidOperationException(
+                    "The directory with the complete example files does not exist " +
+                    $"or is not a directory: {JsonExpectedDir}"
+                );
+            }
+
+            var result = Directory.GetFiles(
+                JsonExpectedDir,
+                "complete.json",
+                System.IO.SearchOption.AllDirectories).ToList();
+            result.Sort();
+
+            return result;
+        }
+
+        private static string Trace(Aas.IClass instance)
+        {
+            switch (instance)
+            {
+                case IIdentifiable identifiable:
+                    {
+                        return $"{identifiable.GetType()} with ID {identifiable.Id}";
+                    }
+                case IReferable referable:
+                    {
+                        return $"{referable.GetType()} with ID-short {referable.IdShort}";
+                    }
+                default:
+                    {
+                        return instance.GetType().Name;
+                    }
+            }
+        }
+
+        class TracingVisitorThrough : Aas.Visitation.VisitorThrough
+        {
+            public readonly List<string> Log = new List<string>();
+
+            public override void Visit(IClass that)
+            {
+                Log.Add(Trace(that));
+                base.Visit(that);
+            }
+        }
+
+        [Test]
+        public void Test_Descend_against_visitor_through()
+        {
+            foreach (string pathToCompleteExample in PathsToCompleteExamples())
+            {
+                IClass? environment = AasCore.Aas3_0_RC02.Jsonization.Deserialize.EnvironmentFrom(
+                    AasCore.Aas3_0_RC02.Tests.CommonJson.ReadFromFile(
+                        pathToCompleteExample));
+
+                if (environment == null)
+                {
+                    throw new System.InvalidOperationException(
+                        "environment unexpectedly null");
+                }
+
+                var logFromDescend = new List<string>();
+
+                foreach (var instance in environment.Descend())
+                {
+                    logFromDescend.Add(Trace(instance));
+                }
+
+                var visitor = new TracingVisitorThrough();
+                visitor.Visit(environment);
+                var traceFromVisitor = visitor.Log;
+
+                Assert.IsNotEmpty(traceFromVisitor);
+
+                Assert.AreEqual(
+                    Trace(environment),
+                    traceFromVisitor[0]);
+
+                traceFromVisitor.RemoveAt(0);
+
+                Assert.That(traceFromVisitor, Is.EquivalentTo(logFromDescend));
+            }
+        }
+
+        [Test]
+        public void Test_against_the_recorded()
+        {
+            string recordingBaseDir = Path.Combine(
+                AasCore.Aas3_0_RC02.Tests.Common.OurTestResourceDir,
+                "Descend");
+
+            foreach (string pathToCompleteExample in PathsToCompleteExamples())
+            {
+                IClass? environment = AasCore.Aas3_0_RC02.Jsonization.Deserialize.EnvironmentFrom(
+                    AasCore.Aas3_0_RC02.Tests.CommonJson.ReadFromFile(
+                        pathToCompleteExample));
+
+                if (environment == null)
+                {
+                    throw new System.InvalidOperationException(
+                        "environment unexpectedly null");
+                }
+
+                var writer = new System.IO.StringWriter();
+                foreach (var instance in environment.Descend())
+                {
+                    writer.WriteLine(Trace(instance));
+                }
+
+                string got = writer.ToString();
+
+                var relativePath = Path.GetRelativePath(
+                    JsonExpectedDir, pathToCompleteExample);
+
+                var expectedPath = Path.Join(
+                    recordingBaseDir, relativePath + ".trace");
+
+                if (AasCore.Aas3_0_RC02.Tests.Common.RecordMode)
+                {
+                    string? parent = Path.GetDirectoryName(expectedPath);
+                    if (parent != null)
+                    {
+                        if (!Directory.Exists(parent))
+                        {
+                            Directory.CreateDirectory(parent);
+                        }
+                    }
+
+                    System.IO.File.WriteAllText(expectedPath, got);
+                }
+                else
+                {
+                    if (!System.IO.File.Exists(expectedPath))
+                    {
+                        throw new System.IO.FileNotFoundException(
+                            $"The file with the recorded trace does not exist: {expectedPath}");
+                    }
+
+                    string expected = System.IO.File.ReadAllText(expectedPath);
+                    Assert.AreEqual(
+                        expected,
+                        got,
+                        $"The expected trace from {expectedPath} does not match the actual one " +
+                        $"for the file {pathToCompleteExample}");
+                }
+            }
+        }
+    }
+}
